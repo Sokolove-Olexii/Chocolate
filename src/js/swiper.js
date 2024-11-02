51eeacb6372bd2db9c5b443ab2bf7eefc0eb4d01
Sokolove-Olexii <ocokolkv.pp@gmail.com>
2024-11-02 12:36:58 -0400

    trying slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,61 +1,61 @@
 new Swiper('.products__slider', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  freeMode: true,
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableonInteraction: true,
+  },
+  speed: 750,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
-    keyboard: {
-      enabled: true,
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 16,
     },
-    freeMode: true,
-    autoplay: {
-      delay: 2000,
-      stopOnLastSlide: false,
-      disableonInteraction: true,
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
     },
-    speed: 750,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-      768: {
-        slidesPerView: 2.5,
-        spaceBetween: 16,
-      },
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 18,
-      },
+  },
+});
+
+new Swiper('.reviews-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  freeMode: true,
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableonInteraction: true,
+  },
+  speed: 750,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
-  });
-  
-  new Swiper('.reviews-slider', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
     },
-    keyboard: {
-      enabled: true,
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
     },
-    freeMode: true,
-    autoplay: {
-      delay: 2000,
-      stopOnLastSlide: false,
-      disableonInteraction: true,
-    },
-    speed: 750,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 16,
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 28,
-      },
-    },
-  });
+  },
+});
